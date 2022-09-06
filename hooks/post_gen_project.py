eeac5a56c2f985154ec481de75d5588dbe92957a
Centraal Soluciones <centraalsoluciones@haceb.com>
2022-09-06 18:21:15 -0500

    simplificar el script pero dejar el error

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,31 +1,14 @@
 import os
-import stat
-import shutil
-import pathlib
+from pathlib import Path
+from shutil import copyfile
 
 
-def force_delete(func, path, _):
-    """Error handler for `shutil.rmtree()` equivalent to `rm -rf`.
-    Usage: `shutil.rmtree(path, onerror=force_delete)`
-    From https://docs.python.org/3/library/shutil.html#rmtree-example
-    """
-    os.chmod(path, stat.S_IWRITE)
-    func(path)
+source = Path(".")
+dest = Path("..")
 
+for f in source.iterdir():
+    copyfile(f, dest.joinpath(f.name))
+    f.unlink()
 
-def rmtree(path):
-    """Remove a directory and all its contents. Like rm -rf on Unix.
-    :param path: A directory path.
-    """
-    shutil.rmtree(path, onerror=force_delete)
-
-
-path = "{{cookiecutter.nombre_notebook}}"
-os.chdir("..")
-p = pathlib.Path("{{cookiecutter.nombre_notebook}}")
-f = p.joinpath("{{cookiecutter.nombre_notebook}}.ipynb")
-f.rename("./{{cookiecutter.nombre_notebook}}.ipynb")
-del p
-del f
-
-rmtree(path)
+os.chdir(dest)
+os.rmdir("{{cookiecutter.nombre_notebook}}")
